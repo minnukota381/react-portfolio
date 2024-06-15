86dcceca8423eb2cb23e2d4dd08ba4a26cb2a792
minnukota381 <minnukota381@gmail.com>
2024-06-15 13:43:13 +0530

    update | navbar-toggler-icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
 import './App.css';
+
 function App() {
   return (
     <div className="App">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,65 @@
-import React from 'react';
-import { FaHome, FaUser, FaBriefcase, FaEnvelope } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { RiContactsLine } from "react-icons/ri";
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { CgFileDocument } from "react-icons/cg";
+import { FaDiagramProject } from "react-icons/fa6";
+import { GrTechnology } from "react-icons/gr";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 import MinnuLogo from './Assets/minnu_logo.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#2">
-          <img src={MinnuLogo} alt="Minnu" /> 
+          <img src={MinnuLogo} alt="Minnu" />
           Minnu
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          onClick={toggleNavbar} 
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#2">
-                <FaHome className="nav-icon" /> Home
+                <AiOutlineHome className="nav-icon" /> Home
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#2">
-                <FaUser className="nav-icon" /> About
+                <AiOutlineUser className="nav-icon" /> About
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#2">
-                <FaUser className="nav-icon" /> Skills
+                <GrTechnology className="nav-icon" /> Skills
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#2">
-                <FaBriefcase className="nav-icon" /> Projects
+                <FaDiagramProject className="nav-icon" /> Projects
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#2">
-                <FaEnvelope className="nav-icon" /> Contact
+                <CgFileDocument className="nav-icon" /> Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#2">
+                <RiContactsLine className="nav-icon" /> Contact
               </a>
             </li>
           </ul>
