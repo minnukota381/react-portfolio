27ed174aecbeacb5bf5de9cd032d28ee258594f6
minnukota381 <minnukota381@gmail.com>
2024-06-16 23:51:05 +0530

    added tsparticles & fixed bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,117 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import './Home.css';
-import Type from './Typed';
 import Minnu from './Assets/minnukota381.JPG';
-import Particle from './Particle';
+import Type from './Typed'
 
 const Home = () => {
-  return (
-    <section className="home" id="home">
-      <Container fluid className="home-content text-center">
-        <Particle />
+  const [init, setInit] = useState(false);
 
-        <Row className="align-items-center">
-          <Col xs={12} className="text-center">
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <div className="home">
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+        <div className="home-content text-center">
+          <div className="text">
             <div className="home-circle">
+              {/* Replace with your image */}
               <img src={Minnu} alt="Minnu" className="img-fluid" />
             </div>
-          </Col>
-        </Row>
-
-        <Row className="text">
-          <Col xs={12} className="text-center">
             <div className="text-one">Hello there,</div>
             <div className="text-two">I'm Minnu</div>
             <div className="text-three">
-              <Type />
+              <div><Type/></div>
             </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <div>Loading particles...</div>; // Optionally, provide a loading state
 };
 
 export default Home;
