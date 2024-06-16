991a9ccd79d0db64cc4725a6f26c0a5391f6a85e
minnukota381 <minnukota381@gmail.com>
2024-06-16 20:55:38 +0530

    added | routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from "./components/Navbar";
+
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
