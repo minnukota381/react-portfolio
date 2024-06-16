44b3373b432fe94f5a3ce936fbc6914067e68247
minnukota381 <minnukota381@gmail.com>
2024-06-16 20:56:04 +0530

    update | typed.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './Home.css';
-import Type from './Typed'
+import Type from './Typed';
 import Minnu from './Assets/minnukota381.JPG';
-
+import Particle from './Particle';
 
 const Home = () => {
   return (
     <section className="home" id="home">
       <div className="home-content text-center">
+        <Particle />
+
         <div className="text">
           <div className="home-circle">
             <img src={Minnu} alt="Minnu" className="img-fluid" />
           </div>
           <div className="text-one">Hello there,</div>
           <div className="text-two">I'm Minnu</div>
-          <div className='text-three'><Type/></div>
-
+          <div className="text-three">
+            <Type />
+          </div>
         </div>
       </div>
     </section>
