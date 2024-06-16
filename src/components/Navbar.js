be4c110eb0a33b7a9858fa531fc394d42e06e2c0
minnukota381 <minnukota381@gmail.com>
2024-06-16 20:56:43 +0530

    fixed navbar for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,9 +16,9 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="#2">
+        <a className="navbar-brand" href="#home">
           <img src={MinnuLogo} alt="Minnu" />
           Minnu
         </a>
@@ -33,32 +33,32 @@ const Navbar = () => {
         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#2">
+              <a className="nav-link" aria-current="page" href="#home">
                 <AiOutlineHome className="nav-icon" /> Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#2">
+              <a className="nav-link" href="#about">
                 <AiOutlineUser className="nav-icon" /> About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#2">
+              <a className="nav-link" href="#skills">
                 <GrTechnology className="nav-icon" /> Skills
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#2">
+              <a className="nav-link" href="#projects">
                 <FaDiagramProject className="nav-icon" /> Projects
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#2">
+              <a className="nav-link" href="#resume">
                 <CgFileDocument className="nav-icon" /> Resume
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#2">
+              <a className="nav-link" href="#contact">
                 <RiContactsLine className="nav-icon" /> Contact
               </a>
             </li>
