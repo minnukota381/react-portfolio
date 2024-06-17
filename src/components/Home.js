57ae881e5ed5d85a03c7b93052edf437864a1cef
minnukota381 <minnukota381@gmail.com>
2024-06-17 13:23:04 +0530

    increased the size type.js font

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import "./Home.css";
 import Minnu from "./Assets/minnukota381.JPG";
 import Type from "./Typed";
@@ -6,20 +7,18 @@ import Type from "./Typed";
 const Home = () => {
   return (
     <div className="home">
-      <div className="home-content text-center">
-        <div className="text">
+      <Row className="home-content text-center">
+        <Col className="text">
           <div className="home-circle">
             <img src={Minnu} alt="Minnu" className="img-fluid" />
           </div>
           <div className="text-one">Hello there,</div>
           <div className="text-two">I'm Minnu</div>
           <div className="text-three">
-            <div>
-              <Type />
-            </div>
+            <Type />
           </div>
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 };
