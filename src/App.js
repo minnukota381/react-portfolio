8fa1d4350f78a51bcc5e975ba4cc226df4a9c125
minnukota381 <minnukota381@gmail.com>
2024-06-17 09:15:54 +0530

    fixed errors in particles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import "./App.css";
+import particlesOptions from "./particles.json";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (init) {
+      return;
+    }
+
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, [init]);
+
   return (
     <Router>
+      {init && <Particles options={particlesOptions} />}
+
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,4 +36,3 @@ function App() {
 }
 
 export default App;
-
