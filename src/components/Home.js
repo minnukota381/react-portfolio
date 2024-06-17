f69c062418a18b70a5724f6067a34b0931fb7f9f
minnukota381 <minnukota381@gmail.com>
2024-06-17 09:17:00 +0530

    added particles.json template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,27 @@
-import React, { useEffect, useState } from "react";
-import Particles from "@tsparticles/react";
-import { particleOptions, initParticles } from "./Particle"; // Adjust path as necessary
+import React from "react";
 import "./Home.css";
 import Minnu from "./Assets/minnukota381.JPG";
 import Type from "./Typed";
 
 const Home = () => {
-  const [init, setInit] = useState(false);
-
-  useEffect(() => {
-    initParticles().then(() => {
-      setInit(true);
-    });
-  }, []);
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-  if (init) {
-    return (
-      <div className="home">
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={particleOptions}
-        />
-        <div className="home-content text-center">
-          <div className="text">
-            <div className="home-circle">
-              <img src={Minnu} alt="Minnu" className="img-fluid" />
-            </div>
-            <div className="text-one">Hello there,</div>
-            <div className="text-two">I'm Minnu</div>
-            <div className="text-three">
-              <div>
-                <Type />
-              </div>
+  return (
+    <div className="home">
+      <div className="home-content text-center">
+        <div className="text">
+          <div className="home-circle">
+            <img src={Minnu} alt="Minnu" className="img-fluid" />
+          </div>
+          <div className="text-one">Hello there,</div>
+          <div className="text-two">I'm Minnu</div>
+          <div className="text-three">
+            <div>
+              <Type />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-
-  return <div>Loading particles...</div>;
+    </div>
+  );
 };
 
 export default Home;
