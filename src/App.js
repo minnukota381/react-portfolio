4b21b7bceaf43d5a09663e8c38e93397e0b56404
minnukota381 <minnukota381@gmail.com>
2024-06-18 11:31:31 +0530

    update | App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Resume from "./components/Resume";
-import Projects from "./components/Projects"; // Import the Projects component
+import Projects from "./components/Projects";
 
 function App() {
   const [init, setInit] = useState(false);
